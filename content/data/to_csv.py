167fe9d56a5aa19ad38bfd23e0a2b8c9938c21a1
PippoZord <pippolor8@gmail.com>
2023-03-08 15:14:40 +0100

    Aggiunto ID nel file Anagrafica per analisi

--- content/data/to_csv.py
+++ content/data/to_csv.py
@@ -1,4 +1,20 @@
 import pandas as pd
 
-read_file = pd.read_csv (r'/home/pippozord/Scrivania/anagrafica.txt')
-read_file.to_csv (r'/home/pippozord/Scrivania/Statistica/2023/superhero-datascience/content/data/anagrafica.csv', index=None)
+read_file = open(r'/home/pippozord/Scrivania/anagrafica.txt')
+
+lines = read_file.readlines();
+list = []
+list.append(lines[0])
+lines = lines[1:]
+i = 1
+
+for line in lines:
+    tmp = str(i);
+    list.append('Giocatore' + tmp + ' ' + line)
+    i = i+1;
+
+print(list[0:3])
+
+with open(r'/home/pippozord/Scrivania/Statistica/2023/superhero-datascience/content/data/Anagrafica.csv', 'w') as write_file:
+    for line in list:
+        write_file.write("%s" %line)    
